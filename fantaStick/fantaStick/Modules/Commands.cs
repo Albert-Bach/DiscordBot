920de8f78331330be9cdb8eaca2a595c5d50e0ec
Albert Bach <bach_albert@hotmail.hu>
2018-01-12 10:14:50 +0100

    feat (fantaSTICK): new commmands

---
+++ fantaStick/fantaStick/Modules/Commands.cs
@@ -0,0 +1,44 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace fantaStick.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Hello World!");
+        }
+
+        [Command("mifajta")]
+        public async Task WhatKindOfAsync()
+        {
+            await ReplyAsync("https://www.youtube.com/watch?v=RaV-TY3ajk4");
+        }
+        
+        [Command("fa")]
+        public async Task WoodAsync()
+        {
+            await ReplyAsync("https://www.youtube.com/watch?v=jrbp0WoIBIs");
+        }
+
+        [Command("hova")]
+        public async Task WhereAsync()
+        {
+            await ReplyAsync("Rácsra!");
+        }
+
+        [Command("rács")]
+        public async Task CageAsync()
+        {
+            await ReplyAsync(" Az egyetlen hely a fejlődésre!");
+        }
+
+        [Command("ez")]
+        public async Task ThisAsync()
+        {
+            await ReplyAsync("Már az.");
+        }
+    }
+}
